bb79ba1cc2353f8cf144a9d88be207fa7d3173f0
Thai Pangsakulyanont <org.yi.dttvb@gmail.com>
2018-06-23 18:07:37 +0700

    :lipstick: Make it bigger

--- src/App.js
+++ src/App.js
@@ -1,22 +1,17 @@
-
-import React, { Component } from 'react';
-import Countdown from './Countdown.js';
-
-
+import React, { Component } from 'react'
+import Countdown from './Countdown.js'
 
 class App extends Component {
   render() {
-    const currentDate = new Date();
-    const year = (currentDate.getMonth() === 11 && currentDate.getDate() > 23) ? currentDate.getFullYear() + 1 : currentDate.getFullYear();
     return (
       <div className="App">
         <div className="App-header">
-        <img src={require('./Heading.png')} width={1200} height={100} />
+          <img src={require('./Heading.png')} width={1200} height={100} />
         </div>
-        <Countdown date={`${year}-12-24 00:00:00`}/>
+        <Countdown date={new Date(2018, 5, 23, 18, 30)} />
       </div>
-    );
+    )
   }
 }
 
-export default App;
+export default App
--- src/Countdown.js
+++ src/Countdown.js
@@ -1,89 +1,97 @@
-import React, { Component } from 'react';
+import React, { Component } from 'react'
 import PropTypes from 'prop-types'
 
 class Countdown extends Component {
   constructor(props) {
-    super(props);
+    super(props)
 
     this.state = {
       min: 0,
-      sec: 0,
+      sec: 0
     }
   }
 
   componentDidMount() {
     // update every second
     this.interval = setInterval(() => {
-      const date = this.calculateCountdown(this.props.date);
-      date ? this.setState(date) : this.stop();
-    }, 1000);
+      const date = this.calculateCountdown(this.props.date)
+      date ? this.setState(date) : this.stop()
+    }, 1000)
   }
 
   componentWillUnmount() {
-    this.stop();
+    this.stop()
   }
 
   calculateCountdown(endDate) {
-    let diff = (Date.parse(new Date(endDate)) - Date.parse(new Date())) / 1000;
+    let diff = (Date.parse(new Date(endDate)) - Date.parse(new Date())) / 1000
 
     // clear countdown when  is reached
-    if (diff <= 0) return false;
+    if (diff <= 0) return false
 
     const timeLeft = {
       min: 0,
       sec: 0
-    };
+    }
 
     // calculate time difference between now and expected date
     if (diff >= 60) {
-      timeLeft.min = Math.floor(diff / 60);
-      diff -= timeLeft.min * 60;
+      timeLeft.min = Math.floor(diff / 60)
+      diff -= timeLeft.min * 60
     }
-    timeLeft.sec = diff;
+    timeLeft.sec = diff
 
-    return timeLeft;
+    return timeLeft
   }
 
   stop() {
-    clearInterval(this.interval);
+    clearInterval(this.interval)
   }
 
-  addLeadingZeros(value) {
-    value = String(value);
+  formatNumber(value) {
+    value = String(value)
     while (value.length < 2) {
-      value = '0' + value;
+      value = '0' + value
     }
-    return value;
+    return value.split('').map((x, i) => (
+      <span style={{ display: 'inline-block', width: '0.7em' }} key={i}>
+        {x}
+      </span>
+    ))
   }
 
   render() {
-    const countDown = this.state;
+    const countDown = this.state
     return (
       <div className="Countdown">
         <span className="Countdown-col">
           <span className="Countdown-col-element">
-            <strong>{this.addLeadingZeros(countDown.min)}</strong>
+            <strong>{this.formatNumber(countDown.min)}</strong>
             <span>Min</span>
           </span>
         </span>
-
         <span className="Countdown-col">
           <span className="Countdown-col-element">
-            <strong>{this.addLeadingZeros(countDown.sec)}</strong>
+            <strong>:</strong>
+          </span>
+        </span>
+        <span className="Countdown-col">
+          <span className="Countdown-col-element">
+            <strong>{this.formatNumber(countDown.sec)}</strong>
             <span>Sec</span>
           </span>
         </span>
       </div>
-    );
+    )
   }
 }
 
 Countdown.propTypes = {
   date: PropTypes.string.isRequired
-};
+}
 
 Countdown.defaultProps = {
   date: new Date()
-};
+}
 
-export default Countdown;
+export default Countdown
