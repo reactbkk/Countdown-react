3edc51baddc2a3d29a37053355b58f76fe26473c
Ranatchai Chernbamrung <ranatchai@cleverse.com>
2018-07-01 15:31:00 +0700

    add .env to contain firebase environment

--- src/firebase.js
+++ src/firebase.js
@@ -1,11 +1,12 @@
 import firebase from 'firebase'
+/* view in .env */
 const config = {
-  apiKey: 'AIzaSyAK68pIjQoUX05IvFxLV5IDDzChK8t1Vns',
-  authDomain: 'react-bangkok-3-countdown.firebaseapp.com',
-  databaseURL: 'https://react-bangkok-3-countdown.firebaseio.com',
-  projectId: 'react-bangkok-3-countdown',
-  storageBucket: 'react-bangkok-3-countdown.appspot.com',
-  messagingSenderId: '409894190660'
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  databaseURL: process.env.REACT_APP_FIREBASE_DATABASE_URL,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID
 }
 
 firebase.initializeApp(config)
